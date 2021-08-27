9036834963acc3f36652b4976a1e8dcb39fd2b3c
Andrea Freeman <afreeman5566@gmail.com>
2021-08-27 13:49:12 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+// Employee as an Object with a name and streetAddress in literal syntax
+
+let employee = {
+    "name": "Sam", 
+    "streetAddress": "11 Broadway",
+}
+
+    function updateEmployeeWithKeyAndValue(employee,key,value){
+        let employeeCopy= {...employee}
+        employeeCopy[key]=value
+
+        return employeeCopy
+    }
+
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+        employee[key]=value
+        return employee
+    }
+
+    function deleteFromEmployeeByKey(employee,key){
+        let employeeCopy= {...employee}
+        delete employeeCopy[key]
+        return employeeCopy
+    }
+
+    function destructivelyDeleteFromEmployeeByKey(employee,key){
+        delete employee[key]
+        return employee
+    }
